277de71ac29cd1b2a311b81c9e19631508eac1db
olexandra0pryshchepa <130248875+olexandra0pryshchepa@users.noreply.github.com>
2024-10-06 16:57:12 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -10,22 +10,39 @@ const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     try {
       const contacts = await getAllContacts();
-      res.json(contacts);
-    } catch (error) {
-      res.status(500).json({ message: "Error fetching contacts", error: error.message });
-    }
-  });
+     res.status(200).json({
+      status: 200,
+      message: "Successfully found contacts!",
+      data: contacts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 500,
+      message: "Error fetching contacts", error: error.message,});
+  }
+});
 
-  app.get('/contacts/:id', async (req, res) => {
+   app.get('/contacts/:id', async (req, res) => {
     const contactId = req.params.id;
     try {
       const contact = await getContactById(contactId);
       if (!contact) {
-        return res.status(404).json({ message: "Contact not found" });
+        return res.status(404).json({
+          status: 404,
+          message: "Contact not found",
+        });
       }
-      res.json(contact);
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}`,
+        data: contact,
+      });
     } catch (error) {
-      res.status(500).json({ message: "Error fetching contact", error: error.message });
+      res.status(500).json({
+        status: 500,
+        message: "Error fetching contact",
+        error: error.message,
+      });
     }
   });
 
